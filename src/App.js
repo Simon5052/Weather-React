e231d8963bc41837e0156574caa5ae72fc453729
Agyare <kofiagyare0000@gmail.com>
2022-07-06 15:47:58 +0000

    fixing bugs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import axios from 'axios'
 
 function App() {
-  const [data, SetData] = useState({});
+  const [data, SetData] = useState({"coord":{"lon":-0.1969,"lat":5.556},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"base":"stations","main":{"temp":299.71,"feels_like":299.71,"temp_min":299.71,"temp_max":299.71,"pressure":1012,"humidity":77,"sea_level":1012,"grnd_level":1009},"visibility":10000,"wind":{"speed":4.67,"deg":200,"gust":4.88},"clouds":{"all":88},"dt":1657118084,"sys":{"country":"GH","sunrise":1657086743,"sunset":1657131496},"timezone":0,"id":2306104,"name":"Accra","cod":""});
   const [location, SetLocation] = useState('');
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f140e774692949a80bd281d14ddb6b4d`
   const searchLocation = (event) => {
@@ -31,23 +31,23 @@ function App() {
       <div className='top'>
         <p>{data.name}</p>
         <div>
-          <h1>{data.main.temp} &#176;</h1>
+          <h1>{data.main?.temp} &#176;</h1>
         </div>
         <div className='description'>
-          <p>{data.weather[0].description}</p>
+          <p>{data.weather[0]?.description}</p>
         </div>
       </div>
       <div className='buttom'>
         <div className='feels'>
-          <p> 30 &#176; </p>
+          <p> {data.main?.feels_like} &#176; </p>
           <p>Feels</p>
         </div>
         <div className='humidity'>
-          <p>{data.main.humidity}%</p>
+          <p>{data.main?.humidity}%</p>
           <p>humidity</p>
         </div>
         <div>
-          <p>17 MPH</p>
+          <p>{data.wind.speed} MPH</p>
           <p> Wind Speed</p>
         </div>
 
