ee81304185ba3aa92f8a57bb3a61440f32892e15
Agyare <kofiagyare0000@gmail.com>
2022-07-07 17:09:30 +0000

    chore(app): fxing error try and catch statement

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import axios from 'axios'
 function App() {
   const [data, SetData] = useState({"coord":{"lon":-0.1969,"lat":5.556},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"base":"stations","main":{"temp":299.71,"feels_like":299.71,"temp_min":299.71,"temp_max":299.71,"pressure":1012,"humidity":77,"sea_level":1012,"grnd_level":1009},"visibility":10000,"wind":{"speed":4.67,"deg":200,"gust":4.88},"clouds":{"all":88},"dt":1657118084,"sys":{"country":"GH","sunrise":1657086743,"sunset":1657131496},"timezone":0,"id":2306104,"name":"Accra","cod":""});
   const [location, SetLocation] = useState('');
-  const [icon, setIcon] = useState('');
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f140e774692949a80bd281d14ddb6b4d`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_API_KEY}`
   const searchLocation = (event) => {
-    if(event.key==='Enter')
-    {
-      axios.get(url)
-      .then((response) => {
-      SetData(response.data)
-      console.log(data);
-    })
+      if(event.key==='Enter')
+        {
+          axios.get(url)
+          .then((response) => {
+          SetData(response.data)
+          console.log(data);
+          }).catch((error) => {
+            alert("Enter a valid city location or check the spelling of the city");
+          })
 
-    }
+        }
+      
+    
+     
     
   }
 
